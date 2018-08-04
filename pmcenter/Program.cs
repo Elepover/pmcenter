71561ff1096710078bef07d31a96c1a100fb5daa
Elepover <elepover@outlook.com>
2018-08-04 13:28:29 +0800

    Initial

---
+++ pmcenter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace pmcenter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static async Task MainAsync(string[] args){
+
+        }
+    }
+}
