bf70b5566c120677599e229c8d1e3ee78ca4783b
Elepover <elepover@outlook.com>
2020-03-28 19:14:11 +0800

    oops

---
+++ pmcenter/CommandLines/NonServiceModeCmdLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+using static pmcenter.Methods;
+
+namespace pmcenter.CommandLines
+{
+    internal class NonServiceModeCmdLine : ICmdLine
+    {
+        public string Prefix => "noservice";
+        public bool ExitAfterExecution => false;
+        public async Task<bool> Process()
+        {
+            Vars.ServiceMode = false;
+            Log("Service mode disabled.");
+            return true;
+        }
+    }
+}
