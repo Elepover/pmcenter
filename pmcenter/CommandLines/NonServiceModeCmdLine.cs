2bf23a1dc0388eed7ab0982a1c7f528d61847351
Elepover <elepover@outlook.com>
2020-04-07 15:25:04 +0800

    remove warning and fix logging colors

--- pmcenter/CommandLines/NonServiceModeCmdLine.cs
+++ pmcenter/CommandLines/NonServiceModeCmdLine.cs
@@ -10,7 +10,9 @@ namespace pmcenter.CommandLines
     {
         public string Prefix => "noservice";
         public bool ExitAfterExecution => false;
+#pragma warning disable CS1998
         public async Task<bool> Process()
+#pragma warning restore CS1998
         {
             Vars.ServiceMode = false;
             Log("Service mode disabled.");
--- pmcenter/Methods/Methods.Log.cs
+++ pmcenter/Methods/Methods.Log.cs
@@ -10,7 +10,7 @@ namespace pmcenter
     {
         public static Dictionary<LogLevel, LogMode> LogTable = new Dictionary<LogLevel, LogMode>()
         {
-            {LogLevel.INFO, new LogMode() {Color = ConsoleColor.Black, Prefix = "[INFO] ", Func = Console.WriteLine}},
+            {LogLevel.INFO, new LogMode() {Color = ConsoleColor.White, Prefix = "[INFO] ", Func = Console.WriteLine}},
             {LogLevel.WARN, new LogMode() {Color = ConsoleColor.Yellow, Prefix = "[WARN] ", Func = Console.WriteLine}},
             {LogLevel.ERROR, new LogMode() {Color = ConsoleColor.Red, Prefix = "[ERROR] ", Func = Console.Error.WriteLine}}
         }; // table-driven
